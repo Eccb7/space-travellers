086f83787ce41e79dc609100a2b212c509f82e9f
lily4178993 <nellytelli@gmail.com>
2023-08-07 13:02:56 +0000

    Create basic container folders

---
+++ src/containers/index.js
@@ -0,0 +1,5 @@
+export { default as Header } from './header/Header';
+export { default as Rockets } from './rockets/Rockets';
+export { default as Missions } from './missions/Missions';
+export { default as Profile } from './profile/Profile';
+export { default as Footer } from './footer/Footer';
---
+++ src/containers/missions/Missions.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './missions.css';
+
+function Missions() {
+  return <div>Missions</div>;
+}
+
+export default Missions;
