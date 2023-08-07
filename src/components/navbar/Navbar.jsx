f9a25ad0b94a421102e3fcfb67c3be534c23f0c7
lily4178993 <nellytelli@gmail.com>
2023-08-07 13:02:24 +0000

    Create basic components folders

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './navbar.css';
+
+function Navbar() {
+  return <nav>Navbar</nav>;
+}
+
+export default Navbar;
---
+++ src/components/rocketFeature/RocketFeature.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './rocketFeature.css';
+
+const rocketFeature = () => (
+  <div>rocketFeature</div>
+);
+
+export default rocketFeature;
