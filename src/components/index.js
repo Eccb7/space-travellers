5964a65a82d02a08d33c34a5365168c7514c6877
lily4178993 <nellytelli@gmail.com>
2023-08-08 23:47:29 +0000

    Removed unused files

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
 export { default as MissionFeature } from './missionFeature/MissionFeature';
-export { default as RocketFeature } from './rocketFeature/RocketFeature';
