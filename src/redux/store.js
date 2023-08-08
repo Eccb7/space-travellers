cfcfd15f51433b0761dc2a64e6932fe5f6d9b069
Briton Otieno <ojwangbriton@gmail.com>
2023-08-08 21:47:17 +0300

    Resolve merge conflict

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { createLogger } from 'redux-logger';
+import missionReducer from './missions/missionSlice';
 import rocketsReducer from './rockets/rocketsSlice';
 
+const logger = createLogger();
 const store = configureStore({
   reducer: {
+    mission: missionReducer,
     rockets: rocketsReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
