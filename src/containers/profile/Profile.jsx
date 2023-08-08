21e5af4ca23c070dc658ad551bd3eadc81879c17
Briton Otieno <ojwangbriton@gmail.com>
2023-08-08 19:22:09 +0300

    Add profile rockets

--- src/containers/profile/Profile.jsx
+++ src/containers/profile/Profile.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './profile.css';
 
 function Profile() {
-  return <div>Profile</div>;
+  const rockets = useSelector((state) => state.rockets.Data.filter((rocket) => rocket.reserved));
+
+  return (
+    <div className="my-profile-container">
+      <h2>My Profile</h2>
+      <div className="sectionTitle">My Rockets</div>
+      {rockets.length === 0 ? (
+        <p>No rockets reserved yet!</p>
+      ) : (
+        <table className="holderTable">
+          <tbody>
+            {rockets.map((rocket) => (
+              <tr key={rocket.id} className="rocketrow">
+                <td className="rocketdata">{rocket.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 }
 
 export default Profile;
