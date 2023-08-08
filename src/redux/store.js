334898587f9d1083c2b504b15b56e7c54f22611f
Briton Otieno <ojwangbriton@gmail.com>
2023-08-08 21:54:29 +0300

    Add file

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { createLogger } from 'redux-logger';
 import missionReducer from './missions/missionSlice';
-// import rocketsReducer from './rockets/rocketsSlice';
 
 const logger = createLogger();
 const store = configureStore({
   reducer: {
     mission: missionReducer,
-    // rockets: rocketsReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
