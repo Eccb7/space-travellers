1fa96a1e3adbebaedcbcdd7803b4b665889ad7c6
Briton Otieno <ojwangbriton@gmail.com>
2023-08-09 22:46:14 +0300

    Update test for Profile component

--- src/__tests__/Profile.test.js
+++ src/__tests__/Profile.test.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store'; // Import redux-mock-store
+import configureStore from 'redux-mock-store';
 import Profile from '../containers/profile/Profile';
 
-const mockStore = configureStore(); // Create a mock store instance
+const mockStore = configureStore();
 
 test('matches snapshot', () => {
-  const store = mockStore({ // Create a mock store with initial state
+  const store = mockStore({
     rockets: { Data: [] },
     mission: { missions: [] },
   });
 
-  // Render the Profile component within the Provider with the mock store
   const { asFragment } = render(
     <Provider store={store}>
       <Profile />
