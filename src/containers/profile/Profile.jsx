309144da8939188f597c12e6f0b407c422a60826
lily4178993 <nellytelli@gmail.com>
2023-08-09 14:52:31 +0000

    Enhance app design

--- src/containers/profile/Profile.jsx
+++ src/containers/profile/Profile.jsx
@@ -18,47 +18,46 @@ function Profile() {
     .filter((mission) => mission.reserved));
 
   return (
-    <div className="my-profile-container">
-      <h2>My Profile</h2>
-      <div className="sectionTitle">My Rockets</div>
-      {rockets.length === 0 ? (
-        <p>No rockets reserved yet!</p>
-      ) : (
-        <table className="holderTable">
-          <tbody>
-            {rockets.map((rocket) => (
-              <tr key={rocket.id} className="rocketrow">
-                <td className="rocketdata">
-                  {rocket.name}
-                  <a href={rocket.wikipedia} target="_blank" rel="noreferrer">Read more</a>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-      {missions.length === 0 ? (<p>No missions joined</p>)
-        : (
+    <section className="my-profile-container">
+      <div>
+        <span className="sectionTitle">My Rockets</span>
+        {rockets.length === 0 ? (
+          <p>No rockets reserved yet!</p>
+        ) : (
           <table className="holderTable">
-            <thead>
-              <tr className="app_mission_profile-row">
-                <th className="app_mission_profile-column">My Missions</th>
-              </tr>
-            </thead>
             <tbody>
-              {
-                missions.map((mission) => (
-                  <MissionProfile
-                    key={mission.mission_id}
-                    missionName={mission.mission_name}
-                    missionMore={mission.wikipedia}
-                  />
-                ))
-              }
+              {rockets.map((rocket) => (
+                <tr key={rocket.id} className="rocketrow">
+                  <td className="rocketdata">
+                    {rocket.name}
+                    <a href={rocket.wikipedia} target="_blank" rel="noreferrer">Read more</a>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         )}
-    </div>
+      </div>
+      <div>
+        <span className="sectionTitle">My Missions</span>
+        {missions.length === 0 ? (<p>No missions joined</p>)
+          : (
+            <table className="holderTable">
+              <tbody>
+                {
+                  missions.map((mission) => (
+                    <MissionProfile
+                      key={mission.mission_id}
+                      missionName={mission.mission_name}
+                      missionMore={mission.wikipedia}
+                    />
+                  ))
+                }
+              </tbody>
+            </table>
+          )}
+      </div>
+    </section>
   );
 }
 
